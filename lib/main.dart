ca65474bb83ce362032482659e97f342d6c82d08
Filipe Veber <FilipeVeber@users.noreply.github.com>
2020-02-12 11:05:38 -0300

    Text composer

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_online/chat_screen.dart';
 
-void main() => runApp(MaterialApp(
-      home: HomePage(),
-    ));
+void main() {
+  runApp(HomePage());
+}
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.red,
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Chat Flutter",
+      theme: ThemeData(
+          primarySwatch: Colors.blue,
+          iconTheme: IconThemeData(color: Colors.blue)),
+      home: ChatScreen(),
     );
   }
 }
